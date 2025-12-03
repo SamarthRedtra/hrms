a9acfedf2cb0efedba6c2fbab4cde8c62b943b44
Redtra Technologies FZE LLC <samarth.upare@redtra.com>
2025-12-03 23:39:23 +0530

    Update hooks.py

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -1,8 +1,8 @@
 app_name = "hrms"
-app_title = "Frappe HR"
-app_publisher = "Frappe Technologies Pvt. Ltd."
+app_title = "Redtra HR"
+app_publisher = "Redtra Technologies FZE LLC"
 app_description = "Modern HR and Payroll Software"
-app_email = "contact@frappe.io"
+app_email = "info@redtra.com"
 app_license = "GNU General Public License (v3)"
 required_apps = ["frappe/erpnext"]
 source_link = "http://github.com/frappe/hrms"
